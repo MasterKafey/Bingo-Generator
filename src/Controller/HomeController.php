19e45b95f4e162b551ede60392a0351eb7bd7282
Jean Marius <jean.marius@dyosis.com>
2021-12-20 22:37:01 +0100

    Initial commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function home(ManagerRegistry $managerRegistry): Response
     {
-        $bingoCards = $managerRegistry->getRepository(BingoCard::class)->findActiveCards();
+        $bingoCards = $managerRegistry->getRepository(BingoCard::class)->findActiveCards($this->getUser());
 
         return $this->render('Page/Home/home.html.twig', [
             'bingo_cards' => $bingoCards,
--- src/Repository/BingoCardRepository.php
+++ src/Repository/BingoCardRepository.php
@@ -3,28 +3,32 @@
 namespace App\Repository;
 
 use App\Entity\BingoCardCell;
+use App\Entity\User;
 use Doctrine\ORM\EntityRepository;
-use function Doctrine\ORM\QueryBuilder;
 
 class BingoCardRepository extends EntityRepository
 {
-    public function findActiveCards(): iterable
+    public function findActiveCards(User $user): iterable
     {
         $queryBuilder = $this
             ->createQueryBuilder('bingo_card')
-            ->innerJoin('bingo_card.cells', 'cell');
+            ->innerJoin('bingo_card.cells', 'cell')
+            ->innerJoin('bingo_card.bingo', 'bingo')
+        ;
 
         $queryBuilder
             ->where(
                 $queryBuilder->expr()->orX(
                     $queryBuilder->expr()->gte('bingo_card.lastUpdateDateTime', ':thirty_minutes'),
                     $queryBuilder->expr()->eq('cell.state', ':uncheck_state')
-                )
+                ) . ' AND ' .
+                $queryBuilder->expr()->eq('bingo.user', ':user')
             );
 
         $queryBuilder
             ->setParameter('thirty_minutes', (new \DateTime())->sub(new \DateInterval('PT30M')))
-            ->setParameter('uncheck_state', BingoCardCell::UNCHECK_STATE);
+            ->setParameter('uncheck_state', BingoCardCell::UNCHECK_STATE)
+            ->setParameter('user', $user);
 
         return $queryBuilder->getQuery()->getResult();
     }
